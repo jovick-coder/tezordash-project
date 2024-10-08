37a1912c1c0da00ca6d3cf1977c93bebff495983
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2024-10-08 22:59:04 +0100

    return to the old code

--- src/app.js
+++ src/app.js
@@ -29,16 +29,14 @@ app.use('/api/v1', payment);
 // deployment
 __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
-  //     app.use(express.static(path.join(__dirname, '/frontend/build')))
+      app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  //     app.get('*', (req, res) => {
-  //         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  //     });
-  app.use(express.static(path.join(__dirname, "../client/build")));
+      app.get("*", (req, res) => {
+        res.sendFile(
+          path.resolve(__dirname, "frontend", "build", "index.html")
+        );
+      });
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-  });
 } else {
     app.get('/', (req, res) => {
         res.send('Server is Running! 🚀');
